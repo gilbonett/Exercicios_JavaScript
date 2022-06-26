16b91f87534005e235156629f653dd6eef25d269
Pedro Bonett <gilbonett@gmail.com>
2022-06-26 13:52:31 -0300

    Exercicio 1/Aula3 de JavaScript terminado

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-var nome = prompt("Qual é teu nome?")
-var sobrenome = prompt("Qual é teu sobrenome?")
-var nacionalidade = prompt("Qual é teu pais de origem")
-var idade = prompt("Qual é tua idade?")
-var cor = prompt("Qual é teu color favorito")
+var dadosPessoais = {
+nome : prompt("Qual é teu nome?"),
+sobrenome : prompt("Qual é teu sobrenome?"),
+nacionalidade : prompt("Qual é teu pais de origem"),
+idade : prompt("Qual é tua idade?"),
+cor : prompt("Qual é teu color favorito")
+}
 
-console.log(`Sou ${nome} ${sobrenome}`);
-console.log (`Sou da ${nacionalidade}, tenho ${idade} anos`);
-console.log(`Minha cor favorita é ${cor}`);
-
-console.table([nome, sobrenome, nacionalidade, idade, cor]);
+console.log(`Ola, sou ${dadosPessoais.nome} ${dadosPessoais.sobrenome}`)
+console.log(`Sou da ${dadosPessoais.nacionalidade}`)
+console.log(`Tenho ${dadosPessoais.idade} anos`)
+console.log(`Minha cor favorita é ${dadosPessoais.cor}`)
+console.table(dadosPessoais);
